759e35bff5074e404637f1555d19ece55c1cb445
sungjin_kim <sungjin_kim@fursys.com>
2026-06-07 17:59:11 +0900

    다호산업 NavBar 에 '재배치 계획' 메뉴 추가

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -19,6 +19,7 @@ const NAV_ITEMS_BY_COMPANY: Record<Company, { href: string; label: string }[]> =
     { href: "/", label: "메인 대시보드" },
     { href: "/paint-line", label: "도장라인 부하" },
     { href: "/package-line", label: "포장1라인 부하" },
+    { href: "/plan", label: "재배치 계획" },
   ],
   대림산업: [
     { href: "/", label: "메인 대시보드" },
